8413c728039b592d4b7b3a1a58a1a7541f7c350b
Nelson Leme <DiscovererXCVIII@gmail.com>
2021-10-04 17:27:33 -0700

    First batch of unit tests.

---
+++ app/src/test/java/com/discoverer/exemplary/viewmodel/MainViewModelTester.kt
@@ -0,0 +1,81 @@
+package com.discoverer.exemplary.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.discoverer.exemplary.api.NetworkHelper
+import com.discoverer.exemplary.model.*
+import com.discoverer.exemplary.util.MainCoroutineRule
+import com.discoverer.exemplary.view.MovieActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+
+
+const val MOVIE_TITLE = "psycho"
+
+
+@RunWith(MockitoJUnitRunner::class)
+class MainViewModelTester {
+
+    @get:Rule
+    val executorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var mockMainRepository: MainRepository
+
+    @Mock
+    private lateinit var mockNetworkHelper: NetworkHelper
+
+    @Mock
+    private lateinit var mockSearchResultsObserver: Observer<Resource<SearchResult>>
+
+    @Mock
+    private lateinit var mockOpenMovieEventObserver: Observer<MovieActivity.Arguments>
+
+    @Mock
+    private lateinit var mockMovieInfoObserver: Observer<Resource<MovieInfo>>
+
+    @Mock
+    private lateinit var mockFoundItem: FoundItem
+
+    private lateinit var mainViewModel: MainViewModel
+
+    @Before
+    fun setup() {
+        mainViewModel = MainViewModel(mockMainRepository, mockNetworkHelper)
+        mainViewModel.searchResults.observeForever(mockSearchResultsObserver)
+        mainViewModel.startMovieEvent.observeForever(mockOpenMovieEventObserver)
+        mainViewModel.movieInfo.observeForever(mockMovieInfoObserver)
+    }
+
+    @Test
+    fun `verify empty title search`() {
+        mainViewModel.fetchMovie("")
+        verify(mockSearchResultsObserver, never()).onChanged(any())
+    }
+
+    @Test
+    fun `verify title search with no network connection`() {
+        `when`(mockNetworkHelper.isNetworkConnected()).thenReturn(false)
+
+        mainViewModel.fetchMovie(MOVIE_TITLE)
+        verify(mockSearchResultsObserver).onChanged(argThat { it.status == Status.ERROR })
+    }
+
+    @Test
+    fun `verify on found item click`() {
+        mainViewModel.onFoundItemClick(mockFoundItem)
+        verify(mockOpenMovieEventObserver).onChanged(argThat { it.foundItem == mockFoundItem })
+    }
+
+} // MainViewModelTester class
